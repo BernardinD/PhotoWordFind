45b6936b5a25381342356248528778c6d6af5c9b
BernardinD <deziusb@knights.ucf.edu>
2023-03-16 01:42:58 -0400

    Manually set username (#19)
    * Initial implementation
    
    * Introducing history of forcibly changed usernames
    
    * Sprucing up implementation
    
    * Final first touches

--- lib/utils/storage_utils.dart
+++ lib/utils/storage_utils.dart
@@ -13,8 +13,9 @@ class SubKeys{
   static String get InstaUsername => "insta";
   static String get AddedOnSnap => "addedOnSnap";
   static String get AddedOnInsta => "addedOnInsta";
-  static String get snapDate => "dateAddedOnSnap";
-  static String get instaDate => "dateAddedOnInsta";
+  static String get SnapDate => "dateAddedOnSnap";
+  static String get InstaDate => "dateAddedOnInsta";
+  static String get PreviousUsernames => "previousUsernames";
 
 }
 
@@ -41,8 +42,12 @@ class StorageUtils {
       SubKeys.InstaUsername: _map[SubKeys.InstaUsername] ?? "",
       SubKeys.AddedOnSnap:   _map[SubKeys.AddedOnSnap] ?? false,
       SubKeys.AddedOnInsta:  _map[SubKeys.AddedOnInsta] ?? false,
-      SubKeys.snapDate:      _map[SubKeys.snapDate]  != null && _map[SubKeys.snapDate].isNotEmpty  ? DateTime.parse(_map[SubKeys.snapDate]).toIso8601String()  : "",
-      SubKeys.instaDate:     _map[SubKeys.instaDate] != null && _map[SubKeys.instaDate].isNotEmpty ? DateTime.parse(_map[SubKeys.instaDate  ]).toIso8601String() : "",
+      SubKeys.SnapDate:      _map[SubKeys.SnapDate]  != null && _map[SubKeys.SnapDate].isNotEmpty  ? DateTime.parse(_map[SubKeys.SnapDate]).toIso8601String()  : "",
+      SubKeys.InstaDate:     _map[SubKeys.InstaDate] != null && _map[SubKeys.InstaDate].isNotEmpty ? DateTime.parse(_map[SubKeys.InstaDate  ]).toIso8601String() : "",
+      SubKeys.PreviousUsernames:     _map[SubKeys.PreviousUsernames] ?? <String, List<String>>{
+        SubKeys.SnapUsername: <String>[],
+        SubKeys.InstaUsername: <String>[],
+      }
     };
     return map;
   }
@@ -50,14 +55,29 @@ class StorageUtils {
   static Future save(String key,
       {String ocrResult,
       @required bool backup,
-        bool reload = false,
-      String snap = "",
-      bool snapAdded, DateTime snapAddedDate}) async {
+      bool reload = false,
+      String snap,
+      bool snapAdded,
+      DateTime snapAddedDate,
+      bool overridingUsername}) async {
+    if (snap != null && overridingUsername == null){
+      throw Exception("Must declare if username is being overwritten");
+    }
+    else if(overridingUsername != null && overridingUsername && snap == null){
+      throw Exception("Missing username to overwrite");
+    }
     Map<String, dynamic> map = await get(key, reload: false, asMap: true);
-    if (ocrResult     != null) map[SubKeys.OCR]             = ocrResult;
-    if (snap          != null) map[SubKeys.SnapUsername]    = snap;
-    if (snapAdded     != null) map[SubKeys.AddedOnSnap]     = snapAdded;
-    if (snapAddedDate != null) map[SubKeys.snapDate]        = snapAddedDate.toIso8601String();
+    if (ocrResult          != null) map[SubKeys.OCR]             = ocrResult;
+    if (snap               != null) map[SubKeys.SnapUsername]    = snap;
+    if (snapAdded          != null) map[SubKeys.AddedOnSnap]     = snapAdded;
+    if (snapAddedDate      != null) map[SubKeys.SnapDate]        = snapAddedDate.toIso8601String();
+    if (overridingUsername != null && overridingUsername) {
+      final List<String> previousUsernames = map[SubKeys.PreviousUsernames][SubKeys.SnapUsername].cast<String>();
+      if (!previousUsernames.contains(snap)) {
+        previousUsernames.add(snap);
+      }
+    }
+
 
     String rawJson = jsonEncode(map);
     if (key != null)
@@ -74,13 +94,7 @@ class StorageUtils {
       {@required bool reload, bool snap = false, bool asMap = false, bool snapAdded = false}) async {
     String rawJson = (await _getStorageInstance(reload: reload)).getString(key);
 
-    Map<String, dynamic> map = convertValueToMap(rawJson);;
-    // try {
-    //   map = json.decode(rawJson);
-    // } on FormatException catch (e) {
-    //   // Assumes this is an OCR that doesn't exist on this phone yet and was created BEFORE format change
-    //   map = await convertValueToMap(rawJson);
-    // }
+    Map<String, dynamic> map = convertValueToMap(rawJson);
 
     if (asMap) {
       return map;
@@ -108,7 +122,7 @@ class StorageUtils {
         // debugPrint("String ($key) matches: ${(value == cloud[key])}");
 
         if (localValue != cloud[key] && isJSON(localValue)) {
-          throw Exception("Cloud and local copies don't match");
+          throw Exception("Cloud and local copies don't match: [$key] \n local: $localValue \n cloud: ${cloud[key]}");
         }
       }
     }
